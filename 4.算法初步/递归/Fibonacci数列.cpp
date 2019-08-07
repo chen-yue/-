fda355e4a33d3f43a36aae7291731a8050de4c16
chen-yue <44504990+chen-yue@users.noreply.github.com>
2019-08-07 16:54:08 +0800

    Update Fibonacci数列.cpp

--- 4.算法初步/递归/Fibonacci数列.cpp
+++ 4.算法初步/递归/Fibonacci数列.cpp
@@ -1,11 +1,32 @@
 #include<cstdio>
-int F(int n)
+//递归方式
+unsigned int F(unsigned int n)
 {
-	if(n==0 || n==1)
+	if(n==1 || n==2)
 		return 1;
 	else
 		return F(n-1)+F(n-2);
 }
+
+//非递归方式
+unsigned int Fib2(unsigned int n)
+{
+    unsigned int nRet, n1, n2;
+    nRet = n1 = n2 = 1;
+
+    if ((n == 1) || (n == 2))
+        return nRet;
+
+    for (unsigned int i = 3; i <= n; i++)
+    {
+        nRet = n1 + n2;
+
+        n1 = n2;
+        n2 = nRet;
+    }
+    return nRet;
+}
+
 int main()
 {
 	int n;
